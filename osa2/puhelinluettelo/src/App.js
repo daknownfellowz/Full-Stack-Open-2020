556d3e846b7a296548a4d3331db673c6002ffde0
daknownfellowz <anssi.syrjala@gmail.com>
2022-01-29 18:53:12 +0200

    Osa 2.6 Puhelinluettelo osa1. Anssi Syrjälä

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      content: newName,  
+      name: newName,
+      key: newName,
+      id: persons.length + 1
+    } 
+    
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>  
+        <div>
+          name:
+          <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+        {persons.map(person => 
+            <Person key={person.id} person={person} />
+        )}
+      
+    </div>
+  )
+}
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+export default App
