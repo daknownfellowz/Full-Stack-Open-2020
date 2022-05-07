90ede600b6e57a974dd906e111b229f93365b743
daknownfellowz <anssi.syrjala@gmail.com>
2022-05-07 20:27:44 +0300

    Osa 2.15 Puhelinluettelo step 7. Anssi Syrjälä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -36,7 +36,13 @@ const App = () => {
         key: newName,
         id: persons.length + 1
       } 
-      
+
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        console.log(response)
+      })
+
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
