6d4cac4857b2a51d2b76c99c40452f3b562be114
daknownfellowz <anssi.syrjala@gmail.com>
2022-05-07 21:04:32 +0300

    Osa 2.16 Puhelinluettelo step 8. Anssi Syrjälä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -7,8 +7,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -37,8 +37,8 @@ const App = () => {
         id: persons.length + 1
       } 
 
-      axios
-      .post('http://localhost:3001/persons', personObject)
+      personService
+      .create(personObject)
       .then(response => {
         setPersons(persons.concat(response.data))
         setNewNumber('')
