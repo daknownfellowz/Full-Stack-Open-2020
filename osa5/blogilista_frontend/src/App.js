9be61edbeaa43f6e427cef471a09d2eb8a9ad7c1
Anssi Syrjälä <anssi.syrjala@gmail.com>
2022-08-22 19:39:28 +0300

    Osa 5.3: blogilistan frontend, step 3. Anssi Syrjälä

--- osa5/blogilista_frontend/src/App.js
+++ osa5/blogilista_frontend/src/App.js
@@ -5,6 +5,9 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -52,6 +55,21 @@ const App = () => {
     setUser(null)
   }
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+    console.log('Add new blog data: ', title, author, url)
+
+    const blog = await blogService.create({
+      title, author, url,
+    })
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
   if (user === null) {
     return (
       <div>
@@ -85,6 +103,39 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={logOut}>logout</button></p>
+
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+      <div>
+        title:
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+          <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+          <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+      </form>  
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
