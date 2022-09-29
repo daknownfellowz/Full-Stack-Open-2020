71b097c2f0d1232d880c4819f9651f2305062880
Anssi Syrjälä <anssi.syrjala@gmail.com>
2022-09-29 22:04:00 +0300

    Osa 5.15: blogilistan testit, step 3. Anssi Syrjälä

--- osa5/blogilista_frontend/src/components/Blog.test.js
+++ osa5/blogilista_frontend/src/components/Blog.test.js
@@ -14,28 +14,44 @@ describe('<Blog/>', () => {
     likes: 100
   }
 
-  beforeEach(() => {
+  test('renders title and author by default', () => {
     container = render(
       <Blog
         blog={blog}
-        deleteBlog={() => console.log('delete')}
-        handleLikes={() => console.log('like')}
+        updateBlog={() => console.log('delete')}
+        removeBlog={() => console.log('like')}
       />
     ).container
-  })
 
-  test('renders title and author by default', () => {
     const div = container.querySelector('.blog')
     expect(div).toHaveTextContent('Component testing is done with react-testing-library')
     expect(div).toHaveTextContent('Jaska Jokunen')
   })
 
+
   test('does not render url and likes by default', () => {
+    container = render(
+      <Blog
+        blog={blog}
+        updateBlog={() => console.log('like')}
+        removeBlog={() => console.log('delete')}
+      />
+    ).container
+
     const div = container.querySelector('.togglableContent')
     expect(div).toHaveStyle('display: none')
   })
 
+
   test('after clicking the button, url and likes are displayed', async () => {
+    container = render(
+      <Blog
+        blog={blog}
+        updateBlog={() => console.log('like')}
+        removeBlog={() => console.log('delete')}
+      />
+    ).container
+
     const user = userEvent.setup()
     const button = screen.getByText('view')
     await user.click(button)
@@ -44,6 +60,23 @@ describe('<Blog/>', () => {
     expect(div).not.toHaveStyle('display: none')
   })
 
-  // screen.debug()
+
+  test('clicking the like button twice calls event handler twice', async () => {
+
+    const mockHandler = jest.fn()
+
+    render(
+      <Blog blog={blog} updateBlog={mockHandler} />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    await user.click(button)
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
+
+  screen.debug()
 
 })
