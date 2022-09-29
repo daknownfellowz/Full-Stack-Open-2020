03efd6330e11cfad07fe449611bdcaad665c1bb0
Anssi Syrjälä <anssi.syrjala@gmail.com>
2022-09-29 21:39:10 +0300

    Osa 5.14: blogilistan testit, step 2. Anssi Syrjälä

--- osa5/blogilista_frontend/src/components/Blog.test.js
+++ osa5/blogilista_frontend/src/components/Blog.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog/>', () => {
+  let container
+
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Jaska Jokunen',
@@ -11,20 +14,34 @@ describe('<Blog/>', () => {
     likes: 100
   }
 
-  const component = render(
-    <Blog blog={blog} />
-  )
-
-  const div = component.container.querySelector('.blog')
+  beforeEach(() => {
+    container = render(
+      <Blog
+        blog={blog}
+        deleteBlog={() => console.log('delete')}
+        handleLikes={() => console.log('like')}
+      />
+    ).container
+  })
 
   test('renders title and author by default', () => {
+    const div = container.querySelector('.blog')
     expect(div).toHaveTextContent('Component testing is done with react-testing-library')
     expect(div).toHaveTextContent('Jaska Jokunen')
   })
 
   test('does not render url and likes by default', () => {
-    expect(div).not.toHaveTextContent('www.bloggeri1.fi')
-    expect(div).not.toHaveTextContent('100')
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, url and likes are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
   })
 
   // screen.debug()
