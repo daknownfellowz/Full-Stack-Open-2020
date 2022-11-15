d944d11e5069471e3c85fe1e5017cf3ef65749f6
Anssi Syrjälä <anssi.syrjala@gmail.com>
2022-11-15 21:51:33 +0200

    Osa 6.15: anekdootit ja backend, step 3. Anssi Syrjälä

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -36,6 +37,13 @@ const anecdoteSlice = createSlice({
     }
   },
 })
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 
 export const { createAnecdote, createVote, setAnecdotes, appendAnecdote } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
