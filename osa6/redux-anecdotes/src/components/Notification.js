dfe6e50d1289f47be34c008b8e347431c6565912
Anssi Syrjälä <anssi.syrjala@gmail.com>
2022-11-16 21:29:57 +0200

    Osa 6.19: anekdootit ja connect, step 1. Osa 6.18: anekdootit ja backend, step 6 oli commitoitu jo aiemmin

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,22 +1,26 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-
-  const notification = useSelector(({ notification }) => {
-    console.log('notifikaatio palaa')
-    return notification
-  })
+const Notification = (props) => {
 
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
+  
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,    
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
